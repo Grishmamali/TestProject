f3a97129cb8c87c9a884eafa2be1760e74981392
Rahul Mahajan <rahul.mahajan@gmail.com>
2022-06-08 16:41:50 -0500

    test2

--- src/employe_detail/EmpDetail.java
+++ src/employe_detail/EmpDetail.java
@@ -9,7 +9,8 @@ public class EmpDetail {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Test");
+		System.out.println("Test 123");
+		
 		 Employee e1= new  Employee();// creating new object for class Employee
 		 e1.setEmp_id(1);
 		 System.out.println("Employe Id: " +e1.getEmp_id());
